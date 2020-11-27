abf63dce182b2b40ac097356ddf8b2914bc051a2
Miro Kropacek <miro.kropacek@gmail.com>
2020-11-27 08:35:42 +0100

    Prepare for Ubuntu installation

--- .travis/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-# -e: Exit immediately if a command exits with a non-zero status.
-# -u: Treat unset variables as an error when substituting.
-# -x: Display expanded script commands
-
-./.travis/install_bintray.sh mintlib
--- .travis/script.sh
+++ .travis/script.sh
@@ -3,12 +3,6 @@
 # -u: Treat unset variables as an error when substituting.
 # -x: Display expanded script commands
 
-export ACLOCAL=aclocal
-export AUTOMAKE=automake
-CC=m68k-atari-mint-gcc RANLIB=m68k-atari-mint-ranlib ./configure --prefix=/ --disable-nls --host=m68k-atari-mint --target=m68k-atari-mint && make
-
-make DESTDIR="${INSTALL_DIR}" install
-rm -rf "${INSTALL_DIR}/m68k-atari-mint"
-
-find "${INSTALL_DIR}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
-find "${INSTALL_DIR}" -type f \( -name '*.a' -o -name '*.o' \) -exec m68k-atari-mint-strip -S -X -w -N '.L[0-9]*' {} \;
+./configure --target=m68k-atari-mint --prefix=/usr --disable-nls
+make
+make install-strip DESTDIR="${INSTALL_DIR}"
